5377f3fcfe644f3dbdc1b88eeb13cf88f94e5dbd
mockcomic <gears2525@aol.com>
2021-09-03 09:38:39 -0700

    Added functionality to purge command
    Added spacing in commands_Handler for readability.
    
    Know issues with Admin_purge.js
    -If given a number more than the amount of messages able to be deleted it will throw and error. It can only delete messages up to 14 days old.

--- commands/Admin_purge.js
+++ commands/Admin_purge.js
@@ -2,26 +2,18 @@
 module.exports = async function (message, arg, command) {
     require("dotenv").config();
     if (message.author.id == process.env.OWNER) {
-
         let number = parseInt(arg) + 1;
         try {
             if (isNaN(number)) {
-                message.reply("That's not a number")
-            } else {
-                message.channel
-                    .fetchMessages({
-                        limit: number
-                    })
-                    .then(messages => {
-                        message.channel.bulkDelete(messages);
-                        message.channel
-                            .send
-                    }).then(message => message.delete(5000));
-            }
-
+                message.reply("That's not a number");
+            } else if (number >100){
+                message.reply("Value larger than 100");
+            }else {
+                message.channel.bulkDelete(number);
+            };
         } catch (error) {
             console.log(error);
             message.channel.send(`Uh oh, error \n ${error}`);
-        }
+        };
     }
 }
