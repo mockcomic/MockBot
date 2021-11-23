d31d527021cbc0a47da2cfef1a417892f41a37a8
Kurtis <gears2525@aol.com>
2021-11-22 19:24:52 -0800

    Removed for security

--- commands/Owner_cmd.js
+++ commands/Owner_cmd.js
@@ -1,21 +1,21 @@
 
-module.exports = async function (message, arg, commands) {    
-    require("dotenv").config();
-    if (message.author.id == process.env.OWNER) {
-        const {exec} = require("child_process");
+// module.exports = async function (message, arg, commands) {    
+//     require("dotenv").config();
+//     if (message.author.id == process.env.OWNER) {
+//         const {exec} = require("child_process");
 
-        exec(`${arg.join(" ")}`, (error, stdout, stderr) => {
-            if (error) {
-                message.channel.send(`error: ${error.message}`);
-                return;
-            }
-            if (stderr) {
-                message.channel.send(`stderr: ${stderr}`);
-                return;
-            }           
-            message.channel.send(`stdout: ${stdout}`);
-        });
-    } else {
-        message.channel.send("⛔Access Denied⛔");
-    };
-}
+//         exec(`${arg.join(" ")}`, (error, stdout, stderr) => {
+//             if (error) {
+//                 message.channel.send(`error: ${error.message}`);
+//                 return;
+//             }
+//             if (stderr) {
+//                 message.channel.send(`stderr: ${stderr}`);
+//                 return;
+//             }           
+//             message.channel.send(`stdout: ${stdout}`);
+//         });
+//     } else {
+//         message.channel.send("⛔Access Denied⛔");
+//     };
+// }
